e6c707965a5a43ddf6950e4b21732aac1672ad7f
Natalie Sandoval <nsandoval@ma.org>
2018-06-18 23:28:24 +0000

    Done.

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,24 @@
 
 # Code your methods below
+def starting_stance
+  puts "Plant your legs far apart"
+def gangnam_dance
+  puts "Lift right foot"
+  puts "Return right foot to the ground"
+  puts "Finish with a small skip-step backward"
+  puts "Lower left foot to the ground"
+  puts "Bounce left foot back up slightly, kicking it a few inches back"
+  puts "Left, right, left, and left"
+  puts "Lower right foot to the ground"
+  puts "Bounce right foot back up slightly, kicking it a few inches back"
+  puts "Right, Left, Right and Right"
+  puts "WHILE doing this:"
+  puts "Hold your arms out in front of you, straight and at chest level"
+  puts "Cross your right wrist over your left and hold them together"
+  puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. Repeat this eight times."
+  puts "Hold left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+  puts "Lift right arm up and point right elbow diagonally to the right"
+  puts "Raise right forearm so taht it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+end
+puts starting_stance
+puts gangnam_dance
